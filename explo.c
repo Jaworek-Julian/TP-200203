98260f40f96d859c84ac7ac7a2428d112a45ca47
Jaworek-Julian <59962629+Jaworek-Julian@users.noreply.github.com>
2020-02-06 14:20:45 +0100

    Choix par nom

--- explo.c
+++ explo.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-    int choix;
+    char pay1[] = "Allemagne", pay2[] = "Norvege", choix[20];
+    int result;
 
     printf("voulez-vous aller en Allemagne (1) ou en Norvege (2)?\n");
-    scanf("%d",&choix);
+    scanf("%s",&choix);
 
-    if (choix == 1){
-        printf("felicitation, vous avez atteint l'Allemagne");
-    }else if (choix == 2){
-        printf("felicitation, vous avez atteint la Norvege");
+    if (strcmp (choix, pay1) == 0){
+        printf("Felicitation, vous avez atteint l'Allemagne\n");
+    }else if (strcmp (choix, pay2) == 0){
+        printf("Felicitation, vous avez atteint la Norvege\n");
     }
 }
